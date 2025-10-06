4c25c39d6912a08d936720c791798e99da79f50b
JarrikAlgera <j.t.algera@student.tudelft.nl>
2025-10-06 10:39:20 +0200

    Per ongelijk in main, maar upload van csvs.

--- load_weather_data.py
+++ load_weather_data.py
@@ -2,6 +2,6 @@ import os
 import pandas as pd
 """This file opens and cleans the datasets for the weather data of KNMI. It stores the data as pandas dataframe in pickle format."""
 
-filename = "weather_2024.txt"
-file_path = os.path.join(os.getcwd(),filename)
+file_weather_stations = "data\locations_weatherstations.csv"
+file_path = os.path.join(os.getcwd(),file_weather_stations)
 print(str(file_path))
